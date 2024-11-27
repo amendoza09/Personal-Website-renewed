2a634879ff777daaf2cbe4c64b5d102f9021e5d7
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-11-27 09:10:53 -0500

    updated website

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+
   return (
-    <React.Fragment>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
-      </Router>
-    </React.Fragment>
+    <Router>
+      <Nav />
+      <div className="container">
+      <TransitionGroup>
+          <CSSTransition
+            timeout={800} // The duration of the transition
+            classNames="fade" // The class name prefix for the transition
+            key={window.location.pathname} // The key for each route (page)
+          >
+            <Routes location={window.location}>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
